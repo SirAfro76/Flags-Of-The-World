aeaf5dd10347a37688127902753ace2ae76244a5
Sir Afro <xtreme.powell@gmail.com>
2024-08-14 09:22:28 +1000

    Slight optimization Made login and signup forms global

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,34 @@
-function loadscriptconfirmation (){
+let loginform;
+let signupform;
+
+function ignite() {
+    loginform = document.getElementById('login-form');
+    signupform = document.getElementById('signup-form');
+}
+ignite();
+
+function loadscriptconfirmation() {
     console.log('Script loaded successfully');
     alert('Script loaded successfully');
 }
 loadscriptconfirmation();
 
-function togglelogin(){
-    const signupform = document.getElementById('signup-form');
-    const loginform = document.getElementById('login-form');
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
 
+        loginform.style.display = "none ";
+        signupform.style.display = "none";
+    };
+});
+
+
+function togglelogin() {
     loginform.style.display = "block";
     signupform.style.display = "none";
 
 }
 
-function togglesignup(){
-    const signupform = document.getElementById('signup-form');
-    const loginform = document.getElementById('login-form');
-
+function togglesignup() {
     signupform.style.display = "block";
     loginform.style.display = "none";
 }
