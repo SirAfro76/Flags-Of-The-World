16642e974cb6560ceadd7248ceffd50f0293ce70
Sir Afro <xtreme.powell@gmail.com>
2024-10-05 00:05:29 +1000

    Got serviceworker to register

--- src/links/js/script.js
+++ src/links/js/script.js
@@ -3,22 +3,10 @@ var signupform = document.getElementById('signup-form');
 var account = document.getElementById('account');
 var pwd = document.getElementById('pwd');
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered with scope: ', registration.scope);
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed: ', error);
-        });
-    });
-  }
-  
-
 function loadscriptconfirmation() {
     console.log('script.js loaded successfully');
-} loadscriptconfirmation();
+}
+loadscriptconfirmation();
 
 document.addEventListener('keydown', function (event) {
     if (event.key === 'Escape') {
@@ -27,25 +15,43 @@ document.addEventListener('keydown', function (event) {
     };
 });
 
+// Open Login Window 
 function toggle_login() {
     loginform.style.display = "block";
     signupform.style.display = "none";
 
 }
 
+// Open Signup Window
 function toggle_signup() {
     signupform.style.display = "block";
     loginform.style.display = "none";
 }
 
+// Show Password Function
 function showpwd() {
     if (pwd.type === "password") {
         pwd.type = "text";
     } else {
-        pwd.type ="password";
+        pwd.type = "password";
     }
 }
-
+// Unfinished
 function drag(obj) {
     obj.dataTransfer.setData()
 }
+
+// Service Worker Registration
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/service-worker.js")
+        .then((registration) => {
+            console.log("Service Worker Registered Successfully:", registration);
+        })
+        .catch((error) => {
+            console.error(`Service worker registration failed: ${error}`);
+        });
+} else {
+    console.log("Service worker is not supported.");
+}
+
